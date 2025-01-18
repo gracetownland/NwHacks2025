7eb2d862414d26d55c2931a240443dedc4692cd4
gracetownland <speak2ayushsrihari@gmail.com>
2025-01-18 14:34:12 -0800

    added tailwind

--- meet-point/src/main.jsx
+++ meet-point/src/main.jsx
@@ -1,9 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './tailwind.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 createRoot(document.getElementById('root')).render(
     <App />
 )
