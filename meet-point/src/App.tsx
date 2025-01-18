0bffe374b76610e177b97b59a6e828e5ad12b912
Mohit Bhagchandani <bh.mohit29@gmail.com>
2025-01-18 15:33:44 -0800

    init again

---
+++ meet-point/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        hi
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
