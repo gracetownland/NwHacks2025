81b3f6a03139f758a96942cf4bb25abaf24fb281
Mohit Bhagchandani <bh.mohit29@gmail.com>
2025-01-18 22:47:32 -0800

    add Map component

---
+++ meet-point/src/Map.tsx
@@ -0,0 +1,36 @@
+import * as turf from "@turf/turf";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useState, useEffect } from "react";
+
+//To make this work, enable location services in your browser and reload
+export default function Map() {
+    const [position, setPosition] = useState<[number, number] | null>(null);
+    const [buffered, setBuffered] = useState<any>(null);
+
+    useEffect(() => {
+        //Need to do it like this because we use the browser's geolocation api which is async
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const newPos: [number, number] = [pos.coords.longitude, pos.coords.latitude];
+            const point = turf.point(newPos);
+            const buffer = turf.buffer(point, 0.5);
+            setPosition(newPos);
+            setBuffered(buffer);
+        });
+    }, []);
+
+    if (!position || !buffered) {
+        //Need this for prefetching dont remove
+        return <div>Loading...</div>;
+    }
+
+    return (
+        //center property does exist lint just doesnt like it idk why
+        <MapContainer center={[position[1], position[0]]} zoom={15} style={{ height: 400, width: "100%" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <GeoJSON data={buffered} />
+        </MapContainer>
+    );
+}
