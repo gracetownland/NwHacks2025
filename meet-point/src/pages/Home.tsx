464a507895b6eca1f0a0dacac427b62e3e3eeab2
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-01-18 18:37:23 -0800

    add Home, Navbar, and Divider components; refactor Header for improved layout

---
+++ meet-point/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { CommunityEvents } from "@/CommunityEvents";
+import Divider from "@/components/Divider";
+import { Header } from "@/Header";
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <Divider />
+      <CommunityEvents />
+    </div>
+  );
+}
+
+export default Home;
