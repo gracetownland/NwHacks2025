7c26892978e5a59226e2370bc70c18ed53cb0fc1
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-01-18 18:36:41 -0800

    implement routing with React Router and update app structure

--- meet-point/src/App.tsx
+++ meet-point/src/App.tsx
@@ -1,16 +1,25 @@
-import "./App.css"
-import { Header } from "./Header"
-import { Carousel } from "./Carousel"
-import { CommunityEvents } from "./CommunityEvents"
+import "./App.css";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { CommunityEvents } from "./CommunityEvents";
+import EventDetail from "./pages/Event/EventDetail";
+import Home from "./pages/Home";
+import { Navbar } from "./Navbar";
+import { Reward } from "./pages/Reward/Rewards";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Carousel />
-      <CommunityEvents />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<CommunityEvents />} />
+        <Route path="/events/:eventId" element={<EventDetail/>} />
+        <Route path="/rewards" element={<Reward />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
